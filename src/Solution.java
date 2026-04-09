baca1706d66511aeea6a244a6dd628eb8aa60098
tve24el006-droid <tve24el006@cet.ac.in>
2026-04-09 18:19:17 +0530

    submitted

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,41 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Solution {
-    public static <T extends Comparable<T>> T findMax(T[] array) {
-        if (array == null || array.length == 0) {
-            return null;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
         }
-        
-        T max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i].compareTo(max) > 0) {
-                max = array[i];
+
+        ListIterator<Integer> iterator = list.listIterator();
+
+        // Forward traversal
+        System.out.print("Forward: ");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next());
+            if (iterator.hasNext()) {
+                System.out.print(" ");
             }
         }
-        return max;
-    }
+        System.out.println();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        Integer[] intArray = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            intArray[i] = sc.nextInt();
+        // Backward traversal
+        System.out.print("Backward: ");
+        while (iterator.hasPrevious()) {
+            System.out.print(iterator.previous());
+            if (iterator.hasPrevious()) {
+                System.out.print(" ");
+            }
         }
+        System.out.println();
 
-        int m = sc.nextInt();
-        String[] stringArray = new String[m];
-        for (int i = 0; i < m; i++) {
-            stringArray[i] = sc.next();
-        }
-
-        System.out.println("Max Integer: " + findMax(intArray));
-        System.out.println("Max String: " + findMax(stringArray));
-        
-        sc.close();
+        scanner.close();
     }
 }
